d2bc847fe48a704012e7b87d51af58009b183a92
Swordry-code <maxencemichel37@gmail.com>
2021-11-21 22:58:24 +0100

    fix slider
    + fix previous slide button

--- website/js/script.js
+++ website/js/script.js
@@ -5,22 +5,28 @@ const precedent = document.querySelector('.right-arrow');
 
 let count = 0;
 
-function slideSuivante()
-{
+function slideSuivante() {
     imgsDiapo[count].classList.remove('active');
 
-    if(count < nbSlide - 1)
-    {
+    if (count < nbSlide - 1) {
         count++;
-        console.log(count);
-    }
-    else count = 0;
+    } else count = 0;
 
     imgsDiapo[count].classList.add('active');
+}
+
+function slidePrecedente() {
+    imgsDiapo[count].classList.remove('active');
     console.log(count);
+    if (count > 0) {
+        count--;
+    } else count = nbSlide - 1;
+    console.log(count);
+    imgsDiapo[count].classList.add('active');
 }
 
 suivant.addEventListener('click', slideSuivante);
+precedent.addEventListener('click', slidePrecedente);
 setInterval(() => {
     slideSuivante();
 }, 6000);
